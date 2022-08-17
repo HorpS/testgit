3eb444ce42f2c9ce90839c21e5ae89c91e2d069e
HorpS <karpetzviktor@gmail.com>
2022-08-17 21:44:38 +0300

    1.0

--- tictac.py
+++ tictac.py
@@ -4,6 +4,17 @@ import time
 player1_choices = []
 player2_choices = []
 
+win_conditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [1, 4, 7], [1, 3, 6], [2, 5, 8]]
+
+win1 = [0, 1, 2]
+win2 = [3, 4, 5]
+win3 = [6, 7, 8]
+win4 = [0, 4, 8]
+win5 = [2, 4, 6]
+win6 = [1, 4, 7]
+win7 = [1, 3, 6]
+win8 = [2, 5, 8]
+
 
 def display(row1, row2, row3):
     print(row1)
@@ -79,8 +90,71 @@ def tictac():
         if i < 5:
             player2()
         i += 1
-        if (0, 1, 2) in player1_choices:
+        result1 = all(elem in player1_choices for elem in win1)
+        if result1:
+            print("You won")
             break
+        result2 = all(elem in player1_choices for elem in win2)
+        if result2:
+            print("You won")
+            break
+        result3 = all(elem in player1_choices for elem in win3)
+        if result3:
+            print("You won")
+            break
+        result4 = all(elem in player1_choices for elem in win4)
+        if result4:
+            print("You won")
+            break
+        result5 = all(elem in player1_choices for elem in win5)
+        if result5:
+            print("You won")
+            break
+        result6 = all(elem in player1_choices for elem in win6)
+        if result6:
+            print("You won")
+            break
+        result7 = all(elem in player1_choices for elem in win7)
+        if result7:
+            print("You won")
+            break
+        result8 = all(elem in player1_choices for elem in win8)
+        if result8:
+            print("You won")
+            break
+        result21 = all(elem in player2_choices for elem in win1)
+        if result21:
+            print("You lost")
+            break
+        result22 = all(elem in player2_choices for elem in win2)
+        if result22:
+            print("You lost")
+            break
+        result23 = all(elem in player2_choices for elem in win3)
+        if result23:
+            print("You lost")
+            break
+        result24 = all(elem in player2_choices for elem in win4)
+        if result24:
+            print("You lost")
+            break
+        result25 = all(elem in player2_choices for elem in win5)
+        if result25:
+            print("You lost")
+            break
+        result26 = all(elem in player2_choices for elem in win6)
+        if result26:
+            print("You lost")
+            break
+        result27 = all(elem in player2_choices for elem in win7)
+        if result27:
+            print("You lost")
+            break
+        result28 = all(elem in player2_choices for elem in win8)
+        if result28:
+            print("You lost")
+            break
+    print("\n")
     print(player1_choices)
     print(player2_choices)
 
